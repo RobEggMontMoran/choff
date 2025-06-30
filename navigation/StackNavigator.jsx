414c44f3eadff62dfada402c417b56ab68db9d52
Rob Moran <rmoran09@qub.ac.uk>
2025-06-30 12:05:01 +0100

    Navigation file added. Placeholder screen names inserted to begin

---
+++ navigation/StackNavigator.jsx
@@ -0,0 +1,32 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LogInScreen from "../screens/LogInScreen";
+import HomeScreen from "../screens/HomeScreen";
+import BeanEntryScreen from "../screens/BeanEntryScreen";
+import BrewEntryScreen from "../screens/BrewEntryScreen";
+import BrewHistoryScreen from "../screens/BrewHistoryScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import AnalyticsScreen from "../screens/AnalyticsScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="LogIn" component={LogInScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="BeanEntry" component={BeanEntryScreen} />
+        <Stack.Screen name="BrewEntry" component={BrewEntryScreen} />
+        <Stack.Screen name="BrewHistory" component={BrewHistoryScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Analytics" component={AnalyticsScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigator;
