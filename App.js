576bb1e85f98819de20a78301310e99961ecf1ea
Rob Moran <rmoran09@qub.ac.uk>
2025-06-30 12:24:01 +0100

    Refactored App.js layout into HomeScreen and set up global navigation

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import StackNavigator from "./navigation/StackNavigator";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <StackNavigator />
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontWeight: "bold",
-    fontSize: 42,
-  },
-});
+export default App;
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 42,
+  },
+});
+
+export default HomeScreen;
